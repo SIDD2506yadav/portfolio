bfa77224a6a1e47f40d796a26cb08a9b157ed1db
Siddhartha <60682055+SIDD2506yadav@users.noreply.github.com>
2026-08-15 19:50:09 +0530

    refactor: define portfolio component boundary

---
+++ src/features/portfolio/components/index.ts
@@ -0,0 +1,7 @@
+export { AboutSection } from "../../../components/portfolio/AboutSection";
+export { CapabilityMap } from "../../../components/portfolio/CapabilityMap";
+export { ContactSection } from "../../../components/portfolio/ContactSection";
+export { ExperienceSection } from "../../../components/portfolio/ExperienceSection";
+export { HeroSection } from "../../../components/portfolio/HeroSection";
+export { ProcessSection } from "../../../components/portfolio/ProcessSection";
+export { WorkSection } from "../../../components/portfolio/WorkSection";
