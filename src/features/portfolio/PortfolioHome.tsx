556f28a86f0b98c43b087e9879940dc1e90da011
Siddhartha <60682055+SIDD2506yadav@users.noreply.github.com>
2026-08-15 19:50:18 +0530

    refactor: consume portfolio feature components

--- src/features/portfolio/PortfolioHome.tsx
+++ src/features/portfolio/PortfolioHome.tsx
@@ -1,10 +1,12 @@
-import { AboutSection } from "../../components/portfolio/AboutSection";
-import { CapabilityMap } from "../../components/portfolio/CapabilityMap";
-import { ContactSection } from "../../components/portfolio/ContactSection";
-import { ExperienceSection } from "../../components/portfolio/ExperienceSection";
-import { HeroSection } from "../../components/portfolio/HeroSection";
-import { ProcessSection } from "../../components/portfolio/ProcessSection";
-import { WorkSection } from "../../components/portfolio/WorkSection";
+import {
+  AboutSection,
+  CapabilityMap,
+  ContactSection,
+  ExperienceSection,
+  HeroSection,
+  ProcessSection,
+  WorkSection,
+} from "./components";
 
 export function PortfolioHome() {
   return (
