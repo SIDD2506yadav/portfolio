110dd24c4759a162f00626ba616aa6a9188334a7
Siddhartha <60682055+SIDD2506yadav@users.noreply.github.com>
2026-08-15 19:48:17 +0530

    refactor: add chat feature public API

---
+++ src/features/chat/index.ts
@@ -0,0 +1,7 @@
+export { ChatProvider, useChat } from "./ChatProvider";
+export type {
+  ChatController,
+  ChatMessage,
+  ChatOpenOptions,
+} from "./types";
+export { sendChatMessage, ChatApiError } from "../../components/portfolio/chatClient";
